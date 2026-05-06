2835de88f8169d60fcc25774201d0ac1ccc1ddf5
admin <admin@admins-MacBook-Pro-2.local>
2026-05-06 20:22:17 +0300

    feat: add console

--- src/sensors/realDht11Sensor.js
+++ src/sensors/realDht11Sensor.js
@@ -6,6 +6,11 @@ const GPIO_PIN = Number(process.env.GPIO_PIN || 4);
 function readSensor() {
   const result = sensor.read(DHT_TYPE, GPIO_PIN);
 
+  console.log("---GPIO_PIN---", GPIO_PIN);
+  console.log(
+    `Read from DHT11: Temp=${result.temperature.toFixed(1)}°C, Humidity=${result.humidity.toFixed(1)}%`,
+  );
+
   return {
     source: "real-dht11",
     temperature: Number(result.temperature.toFixed(1)),
